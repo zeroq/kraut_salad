6cb8802e86ce71bf0a5671165e589432b1e2184b
zeroq <jan-go@gmx.de>
2015-04-03 14:27:11 +0200

    updated observable details to support file objects

--- kraut_parser/utils.py
+++ kraut_parser/utils.py
@@ -1,7 +1,7 @@
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 
 from django.db.models import Q
-from kraut_parser.models import File_Object, HTTPSession_Object, URI_Object, Related_Object, Address_Object
+from kraut_parser.models import File_Object, HTTPSession_Object, URI_Object, Related_Object, Address_Object, Mutex_Object, Code_Object
 
 def get_object_for_observable(observable_type, observable_object=None, object_id=None, no_hash=True):
     """ Return the list of objects belonging to a given observable or the object for a given id.
@@ -11,14 +11,14 @@ def get_object_for_observable(observable_type, observable_object=None, object_id
     """
     return_list = []
     if observable_type == 'FileObjectType':
-        for obj in File_Object.objects.filter(observables=observable_object):
+        for obj in File_Object.objects.filter(Q(observables=observable_object)|Q(id=object_id)):
             if no_hash:
                 return_list.append(obj)
             else:
                 if (obj.md5_hash and obj.md5_hash != 'No MD5') or (obj.sha256_hash and obj.sha256_hash != 'No SHA256'):
                     return_list.append(obj)
     elif observable_type == 'HTTPSessionObjectType':
-        for obj in HTTPSession_Object.objects.filter(observables=observable_object):
+        for obj in HTTPSession_Object.objects.filter(Q(observables=observable_object)|Q(id=object_id)):
             return_list.append(obj)
     elif observable_type == 'URIObjectType':
         for obj in URI_Object.objects.filter(Q(observables=observable_object)|Q(id=object_id)):
@@ -26,6 +26,13 @@ def get_object_for_observable(observable_type, observable_object=None, object_id
     elif observable_type == 'AddressObjectType':
         for obj in Address_Object.objects.filter(Q(observables=observable_object)|Q(id=object_id)):
             return_list.append(obj)
+    elif observable_type == 'MutexObjectType':
+        for obj in Mutex_Object.objects.filter(Q(observables=observable_object)|Q(id=object_id)):
+            return_list.append(obj)
+    elif observable_type == 'CodeObjectType':
+        for obj in Code_Object.objects.filter(Q(observables=observable_object)|Q(id=object_id)):
+            return_list.append(obj)
+
 
     return return_list
 
