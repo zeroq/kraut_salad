1f30fa74787e86892e3fcca605cf31b3dcae9961
zeroq <jan-go@gmx.de>
2015-04-09 23:28:36 +0200

    added help button to quick panel and integrated not yet functional search bar into navigation panel

--- kraut_base/menus.py
+++ kraut_base/menus.py
@@ -4,6 +4,6 @@ from django.core.urlresolvers import reverse
 from menu import Menu, MenuItem
 
 Menu.add_item("main", MenuItem("Home",
-                               reverse("kraut_base.views.home"),
-                               weight=10,
-                               icon='fa-home'))
+    reverse("kraut_base.views.home"),
+    weight=10,
+    icon='fa-home'))
