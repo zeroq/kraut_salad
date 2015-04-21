559ee09e0b5ff51c3057d79d8de09eb363377850
zeroq <jan-go@gmx.de>
2015-04-21 21:05:24 +0200

    added initial campaign details view

--- kraut_intel/urls.py
+++ kraut_intel/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^package/(?P<package_id>\d+)/$', views.package, name='package'),
     url(r'^threatactors/$', views.threatactors, name='threatactors'),
     url(r'^campaigns/$', views.campaigns, name='campaigns'),
+    url(r'^campaign/(?P<campaign_id>\d+)/$', views.campaign, name='campaign'),
     url(r'^indicators/$', views.indicators, name='indicators'),
     url(r'^indicator/(?P<indicator_id>\d+)/$', views.indicator, name='indicator'),
     url(r'^observables/$', views.observables, name='observables'),
