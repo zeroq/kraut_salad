c45dd1b7546e9738adb6b5c6ee28e0b6bfc819f9
zeroq <jan-go@gmx.de>
2015-04-21 21:04:38 +0200

    add basic statistics to entry page

--- kraut_base/views.py
+++ kraut_base/views.py
@@ -3,8 +3,15 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from kraut_parser.models import Package, ThreatActor, Campaign, Indicator, Observable
+
 # Create your views here.
 
 def home(request):
-    context = {}
+    context = {'indicators': 0, 'observables': 0, 'campaigns': 0, 'threatactors': 0, 'packages': 0}
+    context['packages'] = Package.objects.count()
+    context['threatactors'] = ThreatActor.objects.count()
+    context['campaigns'] = Campaign.objects.count()
+    context['indicators'] = Indicator.objects.count()
+    context['observables'] = Observable.objects.count()
     return render_to_response('kraut_base/index.html', context, context_instance=RequestContext(request))
