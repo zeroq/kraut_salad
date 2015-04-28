f59ce1fe4be47a93303c40a1dd0d31ef1ff11629
zeroq <jan-go@gmx.de>
2015-04-28 20:39:02 +0200

    started to work on add contact person modal

--- kraut_incident/forms.py
+++ kraut_incident/forms.py
@@ -1,15 +1,28 @@
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 
 from django.forms import ModelForm, widgets
-from kraut_incident.models import Incident
+from kraut_incident.models import Incident, Contact
+
+class ContactForm(ModelForm):
+    class Meta:
+        model = Contact
+        fields = ["firstname", "lastname", "phone", "email", "description"]
+        widgets = {
+            'firstname': widgets.TextInput(attrs={'id': 'post_contact_firstname', 'class': 'form-control', 'placeholder': '', 'required': True}),
+            'lastname': widgets.TextInput(attrs={'id': 'post_contact_lastname', 'class': 'form-control', 'placeholder': '', 'required': True}),
+            'phone': widgets.TextInput(attrs={'id': 'post_contact_phone', 'class': 'form-control', 'placeholder': ''}),
+            'email': widgets.EmailInput(attrs={'id': 'post_contact_email', 'class': 'form-control', 'placeholder': '', 'required': True}),
+            'description': widgets.Textarea(attrs={'id': 'post_contact_description', 'class': 'form-control', 'placeholder': 'What is this contact responsible for ...'}),
+        }
+
 
 class IncidentForm(ModelForm):
     class Meta:
         model = Incident
         fields = ['incident_number', 'title', 'description', 'status', 'category']
         widgets = {
-            'incident_number': widgets.NumberInput(attrs={'class': 'form-control', 'readonly': True}),
-            'title': widgets.TextInput(attrs={'class': 'form-control', 'placeholder': 'Provide a name for the incident or investigation'}),
+            'incident_number': widgets.NumberInput(attrs={'class': 'form-control', 'readonly': True, 'required': True}),
+            'title': widgets.TextInput(attrs={'class': 'form-control', 'placeholder': 'Provide a name for the incident or investigation', 'required': True}),
             'description': widgets.Textarea(attrs={'class': 'form-control', 'placeholder': 'What and when did it happened ...'}),
             'status': widgets.Select(attrs={'class': 'form-control'}),
             'category': widgets.Select(attrs={'class': 'form-control'}),
--- kraut_incident/views.py
+++ kraut_incident/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib import messages
 
-from kraut_incident.forms import IncidentForm
+from kraut_incident.forms import IncidentForm, ContactForm
 
 # Create your views here.
 
@@ -26,5 +26,7 @@ def new_incident(request):
             messages.info(request, "Incident successfully created!")
         return HttpResponseRedirect(reverse("incidents:new"))
     incident_form = IncidentForm(initial={'status': 1, 'category': 7})
+    contact_form = ContactForm()
     context['formset'] = incident_form
+    context['contact_form'] = contact_form
     return render_to_response('kraut_incident/new.html', context, context_instance=RequestContext(request))
