e6924f9951fa242d45e514b6dc514583bf113097
zeroq <jan-go@gmx.de>
2015-12-30 18:25:12 +0100

    delete and modify taxii servers

--- kraut_sharing/urls.py
+++ kraut_sharing/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^poll/$', views.poll, name='poll'),
     url(r'^servers/$', views.manage_servers, name='servers'),
+    url(r'^server/(?P<server_id>\d+)/delete/$', views.delete_server, name='delete_server'),
 ]
 
--- kraut_sharing/views.py
+++ kraut_sharing/views.py
@@ -29,8 +29,13 @@ def manage_servers(request):
         return render_to_response('kraut_sharing/servers.html', context, context_instance=RequestContext(request))
     serverForm = AddServerForm(request.POST)
     if serverForm.is_valid():
-        new_server = TAXII_Remote_Server.objects.get_or_create(**serverForm.cleaned_data)
-        messages.info(request, 'Successfully added new TAXII server!')
+        oldID = int(request.POST.get('oldid', 0))
+        if oldID == 0:
+            new_server = TAXII_Remote_Server.objects.get_or_create(**serverForm.cleaned_data)
+            messages.info(request, 'Successfully added new TAXII server!')
+        else:
+            old_server = TAXII_Remote_Server.objects.filter(id=oldID).update(**serverForm.cleaned_data)
+            messages.info(request, 'Successfully updated TAXII server!')
     else:
         if serverForm.errors:
             for field in serverForm:
@@ -39,6 +44,17 @@ def manage_servers(request):
     context['form'] = serverForm
     return HttpResponseRedirect(reverse("sharing:servers"))
 
+def delete_server(request, server_id):
+    """ delete TAXII server """
+    try:
+        server = TAXII_Remote_Server.objects.get(pk=int(server_id))
+    except TAXII_Remote_Server.DoesNotExist:
+        messages.error(request, 'The requested TAXII server does not exist!')
+        return HttpResponseRedirect(reverse("sharing:servers"))
+    # TODO: check for associated collections and subscriptions
+    server.delete()
+    messages.info(request, 'The TAXII server was deleted successfully!')
+    return HttpResponseRedirect(reverse("sharing:servers"))
 
 def poll(request):
     """ poll feed information """
