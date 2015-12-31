8ad66fd2ce00173e984a33ca978246b1eb28f059
zeroq <jan-go@gmx.de>
2015-12-31 17:54:28 +0100

    started on poll collection functionality

--- kraut_sharing/feed.py
+++ kraut_sharing/feed.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 
+import dateutil
 import libtaxii as t
 import libtaxii.clients as tc
 from libtaxii.common import generate_message_id
-from libtaxii.messages_11 import CollectionInformationRequest
+from libtaxii.messages_11 import CollectionInformationRequest, PollRequest
 from libtaxii.constants import *
 from dateutil.tz import tzutc
 
@@ -36,6 +37,59 @@ class CollectionRequest:
         taxii_message = t.get_message_from_http_response(http_resp, r.message_id)
         return taxii_message.to_dict()
 
+class CollectionPoll:
+
+    def __init__(self, url, collection_name, begin_timestamp=None, taxii_version=VID_TAXII_XML_11):
+        self.url = urlparse(url)
+        if ':' in self.url.netloc:
+            self.host = self.url.netloc.split(':')[0]
+            self.port = self.url.netloc.split(':')[1]
+        else:
+            self.host = self.url.netloc
+            self.port = None
+        self.collection_name = collection_name
+        self.begin_timestamp = begin_timestamp
+        self.taxii_version = taxii_version
+
+    def handle_response(self, response):
+        """ default response handler. just prints the response """
+        print "Response:\n"
+        print response.to_xml(pretty_print=True)
+        # TODO: handle More=True in response
+
+    def create_client(self):
+        """ create client for connection """
+        client = tc.HttpClient()
+        client.set_use_https(False)
+        # TODO: add authentication stuff
+        return client
+
+    def create_request_message(self):
+        if self.begin_timestamp:
+            begin_ts = dateutil.parser.parse(self.begin_timestamp)
+        # TODO: add support for end timestamp
+        end_ts = None
+        create_kwargs = {
+            'message_id': generate_message_id(),
+            'collection_name': self.collection_name,
+            'exclusive_begin_timestamp_label': begin_ts,
+            'inclusive_end_timestamp_label': end_ts
+        }
+        create_kwargs['poll_parameters'] = PollRequest.PollParameters()
+        poll_req = PollRequest(**create_kwargs)
+        return poll_req
+
+    def run(self):
+        request_message = self.create_request_message()
+        client = self.create_client()
+
+        if self.port:
+            resp = client.call_taxii_service2(self.host, self.url.path, self.taxii_version, request_message.to_xml(pretty_print=True), self.port)
+        else:
+            resp = client.call_taxii_service2(self.host, self.url.path, self.taxii_version, request_message.to_xml(pretty_print=True))
+        r = t.get_message_from_http_response(resp, '0')
+        self.handle_response(r)
+
 
 def main():
     """ Send a Feed Information Request to a Taxii 1.1 Server """
@@ -43,5 +97,9 @@ def main():
     res = script.run()
     print res
 
+def main2():
+    script = CollectionPoll(url='http://hailataxii.com/taxii-discovery-service/', collection_name='guest.Abuse_ch', begin_timestamp='2015-12-20T13:13:48.751691+0000')
+    script.run()
+
 if __name__ == "__main__":
-    main()
+    main2()
