e0053f92a22d20333696e8a3abb0287334c7fc9a
zeroq <jan-go@gmx.de>
2019-05-01 18:21:51 +0200

    added quick search

--- kraut_search/views.py
+++ kraut_search/views.py
@@ -17,13 +17,19 @@ def base_search(request):
         return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
     sterm = request.POST.get('searchbar', None)
     context['sterm'] = sterm
-    ### search file objects
+    ### search observable objects
     queryset = Observable.objects.filter(
         Q(file_object__md5_hash=sterm)|
         Q(file_object__sha256_hash=sterm)|
-        Q(file_object__file_meta__file_name=sterm)
+        Q(file_object__file_meta__file_name=sterm)|
+        Q(uri_object__uri_value=sterm)|
+        Q(address_object__address_value=sterm)|
+        Q(mutex_object__mutex_name=sterm)|
+        Q(driver_object__driver_name=sterm)|
+        Q(link_object__link=sterm)|
+        Q(code_object__custom_value=sterm)|
+        Q(emailmessage_object__subject=sterm)
     ).order_by('-last_modified')
     for item in queryset:
-        context['result'].append( (item.name, item.observable_type) )
-    ### search domain objects
+        context['result'].append( (item.name, item.observable_type, item.id) )
     return render(request, 'kraut_search/index.html', context)
